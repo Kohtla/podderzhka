bf86406fe1e58ec5285fc99af1a792c80c67c46a
Kohtla <aklimakov@nvidia.com>
2023-05-04 18:35:58 +0400

    Some investors info

--- src/views/InvestorsView.vue
+++ src/views/InvestorsView.vue
@@ -1,5 +1,9 @@
 <template>
-  <main>
-    <h1>Investors</h1>
+  <main class="flex justify-center">
+    <div class="mt-10 px-8 py-8 bg-blue rounded-xl">
+      <h1 class="font-inter font-black text-2xl text-white">Dear Investors</h1>
+      <p class="text-white mt-5">Currently we are not looking for investments</p>
+      <p class="text-white mt-5">We will prepare our first investment plan after releasing 'Just Date'</p>
+    </div>
   </main>
 </template>
