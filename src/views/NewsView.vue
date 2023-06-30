1497472745d3dbfb5b99c0d3080fa46b685a4e7a
Kohtla <aklimakov@nvidia.com>
2023-06-30 18:14:15 +0400

    Some news and more contacts

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -4,6 +4,15 @@
 
       <div class="flex-1 py-2 px-2 my-2 mx-2">
         <h1 class="font-inter font-black text-white text-5xl">News</h1>
+
+        <div class="mt-8">
+          <p class="font-inter text-white text-l">15.06.23</p>
+          <p class="font-inter font-black text-white text-2xl">Just Date is now on Steam!</p>
+          <p class="font-inter text-white text-xl">
+            <a class="underline" href="https://store.steampowered.com/app/2417100/Just_Date/">Visit our steam page!</a>
+             Feel free to add the game to your wishlist.
+          </p>
+        </div>
         
         <div class="mt-8">
           <p class="font-inter text-white text-l">11.05.23</p>
@@ -11,10 +20,10 @@
           <p class="font-inter text-white text-xl">Also we’ve met some production problem. The release will be delayed until autumn.</p>
         </div>
 
-        <div class="mt-16">
+        <div class="mt-8">
           <p class="font-inter text-white text-l">10.01.23</p>
           <p class="font-inter font-black text-white text-2xl">A new beginning</p>
-          <p class="font-inter text-white text-xl">We’ve started to create the game, we are planning to release it this summer.</p>
+          <p class="font-inter text-white text-xl">We’ve started creating the game, and we are planning to release it this summer.</p>
         </div>
       </div>
     </div>
